a7f4798a5df1bce7dbd4f59892a7a6183c585fc2
Christopher Hassø <christopher@hassoe.dk>
2025-03-07 11:33:39 +0100

    allow for spectral entropy reguralization to be computed for weight matrices with 2+ dimensions

--- spectral_entropy_regularization/spectral_entropy_loss.py
+++ spectral_entropy_regularization/spectral_entropy_loss.py
@@ -2,11 +2,19 @@ import torch
 import torch.nn as nn
 
 class SpectralEntropyLoss(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        maximum_dimension: int = 2,
+    ):
         """
         Spectral Entropy Loss for regularization.
+        
+        Args:
+            maximum_dimension (int): Maximum dimension of weight matrices for which to compute spectral entropy. Defaults to 2.
         """
         super(SpectralEntropyLoss, self).__init__()
+        assert maximum_dimension >= 2, "The maximum dimension must be at least 2."
+        self.maximum_dimension = maximum_dimension
 
     def spectral_entropy(self, weight_matrix) -> torch.Tensor:
         """
@@ -22,7 +30,7 @@ class SpectralEntropyLoss(nn.Module):
         singular_values = torch.linalg.svdvals(weight_matrix)
         
         # Normalize singular values to form a probability distribution
-        normalized_singular_values = singular_values / torch.sum(singular_values)
+        normalized_singular_values = singular_values / torch.sum(singular_values, dim=1).unsqueeze(1)
         
         # Compute spectral entropy
         spectral_entropy = -torch.sum(normalized_singular_values * torch.log(normalized_singular_values + torch.finfo(torch.float32).eps))
@@ -31,7 +39,7 @@ class SpectralEntropyLoss(nn.Module):
 
     def forward(self, model: nn.Module) -> torch.Tensor:
         """
-        Compute the total spectral entropy of all 2D weight matrices in the model.
+        Compute the total spectral entropy of all weight matrices with dimensions less than or equal to the maximum dimension.
         
         Args:
             model (nn.Module): The neural network model.
@@ -43,7 +51,8 @@ class SpectralEntropyLoss(nn.Module):
         
         # Iterate over all parameters in the model
         for _, param in model.named_parameters():
-            if param.ndim == 2:  # Only apply to 2D weight matrices
-                total_spectral_entropy += self.spectral_entropy(param)
+            if param.ndim > 1 and param.ndim <= self.maximum_dimension:
+                *_, row, col = param.shape
+                total_spectral_entropy += self.spectral_entropy(param.reshape(-1, row, col))
         
         return total_spectral_entropy
